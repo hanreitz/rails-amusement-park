85d138d1e6fa44753afe4081edc5e4aaa35c04be
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-26 19:36:05 -0400

    commit tree 5d2a11a69737b7fafcd61ffa169b1d2045bb392f

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,49 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if can_ride
+      subtract_tickets
+      adjust_mood
+      "Thanks for riding the #{attraction.name}!"
+    elsif enough_tickets && !tall_enough
+      "Sorry. #{not_tall_enough}."
+    elsif tall_enough && !enough_tickets
+      "Sorry. #{not_enough_tickets}."
+    else
+      "Sorry. #{not_enough_tickets}. #{not_tall_enough}."
+    end
+  end
+
+  def subtract_tickets
+    current_tickets = user.tickets.to_i - attraction.tickets.to_i
+    user.update(tickets: current_tickets)
+  end
+
+  def adjust_mood
+    current_happiness = user.happiness.to_i + attraction.happiness_rating.to_i
+    current_nausea = user.nausea.to_i + attraction.nausea_rating.to_i
+    user.update(happiness: current_happiness, nausea: current_nausea)
+  end
+
+  def tall_enough
+    user.height.to_i >= attraction.min_height.to_i
+  end
+
+  def not_tall_enough
+    "You are not tall enough to ride the #{self.attraction.name}"
+  end
+
+  def enough_tickets
+    user.tickets.to_i >= attraction.tickets.to_i
+  end
+
+  def not_enough_tickets
+    "You do not have enough tickets to ride the #{self.attraction.name}"
+  end
+
+  def can_ride
+    tall_enough && enough_tickets
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :rides
+  resources :attractions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  root 'users#home'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 end
